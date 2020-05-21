9c4b35c044418742faeb6cb1d8e758ae88baeeda
yo972358 <namechenjiabin@icloud.com>
2020-05-21 23:47:31 +0800

    237

---
+++ CPP/237删除链表中的节点/deleteNode.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* p = node->next;
+        node->next = p->next;
+        node->val = p->val;
+        delete p;
+    }
+};
